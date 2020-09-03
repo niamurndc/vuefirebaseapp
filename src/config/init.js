d534734dc000da8587e6547527fb7664fc346734
Niamur Rahman <52600388+niamurndc@users.noreply.github.com>
2020-09-03 12:30:16 +0600

    update firebase init

--- src/config/init.js
+++ src/config/init.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfeNuk2RoH2dvuMrCuWJX32F7w5Qv01Fk",
-  authDomain: "employeesvue.firebaseapp.com",
-  databaseURL: "https://employeesvue.firebaseio.com",
-  projectId: "employeesvue",
-  storageBucket: "employeesvue.appspot.com",
-  messagingSenderId: "159796448607",
-  appId: "1:159796448607:web:229944370334c10b3c7aea"
+  your firebase credantials.
 };
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
